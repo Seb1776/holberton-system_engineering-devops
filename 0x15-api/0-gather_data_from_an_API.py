05e30332d3bf97a3a62f510dc01a289ee1678076
Seb1776 <sebastianwho1776@gmail.com>
2021-09-14 04:22:39 +0000

    Added point 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''URL API from JSON bait'''
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+
+    user = requests.get(url + 'users/{}'.format(int(argv[1]))).json()
+    all = requests.get(url + 'todos?userId={}'.format(int(argv[1]))).json()
+
+    response = []
+
+    for task in all:
+        if task.get('completed') is True:
+            response.append(task.get('title'))
+
+    print('Employee {} is done with tasks({}/{}):'.
+          format(user.get('name'), len(response), len(all)))
+
+    for task in response:
+        print('\t {}'.format(task))
