4a7e52a1bda43e80f9f72b637e3fbc3464ad91b9
Seb1776 <sebastianwho1776@gmail.com>
2021-09-30 02:07:35 +0000

    Added point 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Get subs from reddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """[get a parameter of subreddit]
+        subreddit: param to argv[1]
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+
+    header = {
+        'User-Agent':  'Chrome/66.0.3359.139 Mobile Safari/537.36'
+    }
+
+    response = requests.get(url, headers=header)
+
+    if response.status_code == 404:
+        return 0
+
+    res = response.json().get('data')
+    return res.get('subscribers')
